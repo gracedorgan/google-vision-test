cfa4d60050ad895c82f3c0a1f26b7bb198bbbc87
Grace <Grace.M.Dorgan.21@dartmouth.edu>
2019-04-11 21:40:17 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('starting!');
+/* eslint-disable no-unused-vars */
+import $ from 'jquery';
+
+import './style.scss';
+
+const mainel = document.getElementById('main');
+let seconds = 0;
+function incrementSeconds() {
+  seconds += 1;
+  mainel.innerText = `You have been here for ${seconds} seconds.`;
+}
+setInterval(incrementSeconds, 1000);
